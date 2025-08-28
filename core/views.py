50e14b500699544a2981a039fb598a5d5b7b295c
Crosline <cagkancaglayanel@gmail.com>
2025-08-28 17:53:10 +0300

    Add: views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import JobPosting, Applicant, Application
+from .serializers import JobPostingSerializer, ApplicantSerializer, ApplicationSerializer
 
-# Create your views here.
+class JobPostingViewSet(viewsets.ModelViewSet):
+    queryset = JobPosting.objects.all().order_by('-created_at')
+    serializer_class = JobPostingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class ApplicantViewSet(viewsets.ModelViewSet):
+    queryset = Applicant.objects.all()
+    serializer_class = ApplicantSerializer
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = ApplicationSerializer
+
+    def get_queryset(self):
+        queryset = Application.objects.all()
+        job_id = self.request.query_params.get('job_id')
+        applicant_name = self.request.query_params.get('applicant')
+
+        if job_id is not None:
+            queryset = queryset.filter(job__id=job_id)
+
+        if applicant_name is not None:
+            queryset = queryset.filter(applicant__name__icontains=applicant_name)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save()
+
