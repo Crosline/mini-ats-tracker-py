e93a34282d87d410d56a87b77ec051078a3f9635
Crosline <cagkancaglayanel@gmail.com>
2025-09-01 01:34:02 +0300

    Add: base jwt settings and functionality

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets, permissions, generics
+from rest_framework import viewsets, permissions, generics, response, status
 from drf_spectacular.utils import extend_schema_view, extend_schema
 from django.contrib.auth.models import User
 from .models import JobPosting, Applicant, Application
 from .serializers import JobPostingSerializer, ApplicantSerializer, ApplicationSerializer, UserRegistrationSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # --- Registration ViewSet ---
 @extend_schema(tags=['Registration'])
@@ -10,6 +11,23 @@ class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
     permission_classes = [permissions.AllowAny]
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        data = {
+            'user': serializer.data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+        res = response.Response(data, status=status.HTTP_201_CREATED)
+        res.set_cookie('refresh_token', str(refresh.access_token), httponly=True)
+
+        return res
 
 # --- Core Model ViewSets ---
 @extend_schema(tags=['JobPosting'])
